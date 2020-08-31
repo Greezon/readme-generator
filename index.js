efc532197724b141ea1fa42fbe0852040ae5cea0
Ian A <iastesana@gmail.com>
2020-08-31 16:44:12 -0700

    Started the project, installed inquirer and have the pase line of prompts done.

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer")
+
+const inquirer = require ("inquirer")
+inquirer.promp([
+    {
+        type: "input",
+        name: "title",
+        message: "enter project title"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "enter github user-name"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "enter project title"
+    }
+])
