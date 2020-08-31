1b2006cf4bb15fbd72bb478613699f4db2012090
Ian A <iastesana@gmail.com>
2020-08-31 16:55:40 -0700

    Added Description, Table of Contents, Installation, Usage, License, Contributing, Tests

--- index.js
+++ index.js
@@ -9,12 +9,53 @@ inquirer.promp([
     },
     {
         type: "input",
+        name: "developer name",
+        message: "enter developer name"
+    },
+    {
+        type: "input",
         name: "github",
         message: "enter github user-name"
     },
     {
         type: "input",
-        name: "title",
-        message: "enter project title"
+        name: "description",
+        message: "enter project description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "enter installation directions"
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "enter testing methods"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "enter user restrictions"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "license type", 
+        choices: [ 
+            "MIT",
+            "ISC",
+            "N/A",
+            "APACHE 2.0"
+        ]
+    },
+    {
+        type: "input",
+        name: "e-mail",
+        message: "enter e-mail"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "add all contributors"
     }
 ])
